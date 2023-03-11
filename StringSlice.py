0feac83d0f8fdc9d6845a56b6f5caccf4997fcc5
mominadnan63 <mominadnan63@gmail.com>
2023-03-11 18:05:36 +0530

    First Commit

---
+++ StringSlice.py
@@ -0,0 +1,15 @@
+#  01234567890123456    +
+s="Python Programing"
+#  76543210987654321    -
+
+print(s[3:13])
+print(s[:15])
+print(s[5:])
+print(s[2:16:3])
+print(s[::5])
+
+print(s[-15:-5])
+print(s[:-3])
+print(s[-13:])
+print(s[-17:-1:4])
+print(s[::-3])
